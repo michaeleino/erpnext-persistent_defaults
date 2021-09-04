6e771279375120fe8a0cdcda6413114f32fa38db
michaeleino <michaeleino@hotmail.com>
2021-09-04 14:35:06 +0200

    4th fixes

--- persistent_defaults/config/docs.py
+++ persistent_defaults/config/docs.py
@@ -2,8 +2,8 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/customstyle"
-# docs_base_url = "https://[org_name].github.io/customstyle"
+# source_link = "https://github.com/[org_name]/persistent_defaults"
+# docs_base_url = "https://[org_name].github.io/persistent_defaults"
 # headline = "App that does everything"
 # sub_heading = "Yes, you got that right the first time, everything"
 
